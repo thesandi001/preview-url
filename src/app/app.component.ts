3ea011eae1a67aa5e947ad42eff284aaa6a067d8
Sandipan Mitra <sandipan.mitra001@gmail.com>
2019-10-13 01:11:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { urlPreview } from 'url-preview';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'preview-url';
+  url;
+  //https://www.livemint.com/companies/news/piramal-enterprises-dispels-rumours-about-financial-services-business-files-complaint-11570699923572.html
+  response;
+
+  ngOnInit() {  	
+  	
+  }
+
+  urlFetch() {
+  	urlPreview(this.url).then(data => this.response = data);
+  }
 }
